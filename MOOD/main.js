c0c9d4c141fcb374b5e9b89e3cf9537d07d83ff9
privat-jeyk <n2ytmt7wr8@privaterelay.appleid.com>
2026-06-14 08:04:03 +0200

    push

--- MOOD/main.js
+++ MOOD/main.js
@@ -1,3 +1,14 @@
 /* GET THE DATE */
 var date = new Date();
 console.log(date);
+
+/* EXTRACT THE CURRENT DATE INFO */
+var currentMonth = date.getMonth();
+var currentDay = date.getDay();
+var currentDate = date.getDate();
+var currentYear = date.getFullYear();
+
+console.log(currentMonth);
+console.log(currentDay);
+console.log(currentDate);
+console.log(currentYear);
