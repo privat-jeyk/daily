94c6cc8c0d65e1e39960e4d7c7b7ad02107863ae
privat-jeyk <n2ytmt7wr8@privaterelay.appleid.com>
2026-09-13 07:53:47 +0200

    push

---
+++ slots/js/game.js
@@ -0,0 +1,58 @@
+function init() {
+
+}
+
+// Rückgabe: Array mit den Slot-Instanzen
+function createSlots() {
+
+}
+
+// Setzt das Drehen der Slots in Gang
+function spin() {
+
+}
+
+// Liefert Zufallszahl inkl. min und max
+function getRandomInt(min, max) {
+    
+}
+
+// Rückgabe: true oder false, je nachdem ob Drehung in Gang
+function isStillSpinning() {
+    
+}
+
+// Auswertung des Spielergebnisses
+function evaluateRound() {
+    
+}
+
+// Gewonnen
+function won(points) {
+    
+}
+
+// Verlorem
+function lose() {
+    
+}
+
+// Drehbutton ein und ausschalten
+function setSpinButton( active ) {
+    setSpinButton.disabled = !active;
+}
+
+// Infotext
+function setInfoText(txt) {
+    setInfoText.innerText = txt;
+}
+
+// Scoretext
+function setScoreText(txt) {
+    setScoreText.innerText = txt;
+}
+
+// Aktualisiert das Spiel regelmässig
+function update() {
+    
+}
